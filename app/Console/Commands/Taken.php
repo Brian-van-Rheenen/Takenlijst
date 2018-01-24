7f9378ba1a1383f640dd093d216ef945db63265a
Brian van Rheenen <brian@moodles.nl>
2018-01-24 14:22:23 +0100

    Bot now PMs the assigned user.

--- app/Console/Commands/Taken.php
+++ app/Console/Commands/Taken.php
@@ -64,48 +64,22 @@ class Taken extends Command
 
             if (sizeof($werknemers) > 0)
             {
-                // Create a constant to store your Slack URL
-                if (!defined('SLACK_WEBHOOK')) define('SLACK_WEBHOOK', 'https://hooks.slack.com/services/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');
-
-                if (sizeof($werknemers) < 2)
+                foreach ($werknemers as $werknemer)
                 {
-                    // Make your message
-                    $message = array('payload' => json_encode(array('text' => '*' . $werknemers[0][0]->naam . '* moet: ' . $taak->taak . '.')));
+                    // // Create a constant to store your Slack URL
+                    if (!defined('SLACK_WEBHOOK')) define('SLACK_WEBHOOK', $werknemer[0]->webhook);
+
+                    //Make your message
+                    $message = array('payload' => json_encode(array('text' => 'Vergeet je taak niet!: *' . $taak->taak . '*.')));
+
+                    // Use curl to send your message
+                    $c = curl_init(SLACK_WEBHOOK);
+                    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($c, CURLOPT_POST, true);
+                    curl_setopt($c, CURLOPT_POSTFIELDS, $message);
+                    curl_exec($c);
+                    curl_close($c);
                 }
-                else
-                {
-                    $namen = null;
-
-                    foreach ($werknemers as $werknemer)
-                    {
-                        if($namen === null)
-                        {
-                            $namen = $werknemer[0]->naam;
-                        }
-                        else
-                        {
-                            if($werknemer == end($werknemers))
-                            {
-                                $namen = $namen . ' en ' . $werknemer[0]->naam;
-                            }
-                            else
-                            {
-                                $namen = $namen . ', ' . $werknemer[0]->naam;
-                            }
-                        }
-                    }
-
-                    // Make your message
-                    $message = array('payload' => json_encode(array('text' => '*' . $namen . '* moeten: ' . $taak->taak . '.')));
-                }
-
-                // Use curl to send your message
-                $c = curl_init(SLACK_WEBHOOK);
-                curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($c, CURLOPT_POST, true);
-                curl_setopt($c, CURLOPT_POSTFIELDS, $message);
-                curl_exec($c);
-                curl_close($c);
             }
         }
     }
